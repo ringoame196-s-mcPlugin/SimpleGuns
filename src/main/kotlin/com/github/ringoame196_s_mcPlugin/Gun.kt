507ce7dd05e124743fa628843d449a4188b9c22b
RIngoame196 <132573268+Ringoame196@users.noreply.github.com>
2026-07-25 21:05:49 +0900

    feat ダメージを与える処理、クラスに銃の情報をもたせる

--- src/main/kotlin/com/github/ringoame196_s_mcPlugin/Gun.kt
+++ src/main/kotlin/com/github/ringoame196_s_mcPlugin/Gun.kt
@@ -3,6 +3,9 @@ package com.github.ringoame196_s_mcPlugin
 import org.bukkit.entity.Player
 
 interface Gun {
+    val firingRangeDistance: Double
+    val damage: Double
+
     fun shot(player: Player)
     fun reload()
 }
